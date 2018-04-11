6432232bdb0750943539d3f8222d2fd7f33a361b
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2018-04-11 19:53:52 -0300

    added browser sync plugin and clone myxyapp project for template

--- www/js/index.js
+++ www/js/index.js
@@ -42,5 +42,123 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
 app.initialize();
+
+function OnOff() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.addEventListener("readystatechange", function() {
+        // console.log(this.readyState);
+        if (this.readyState === 4) {
+            $('#alert').fadeIn();
+        }
+        if (this.readyState === 404) {
+            alert('Error de conexión');
+        }
+    });
+    xmlhttp.open("GET", "http://192.168.4.1/LED=ONOFF");
+    xmlhttp.send();
+}
+
+function newOnoff() {
+    var request;
+    if (request) {
+        return;
+    }
+    request = $.ajax({
+        url: 'http://192.168.4.1/LED=ONOFF/',
+        data: {},
+        type: 'GET',
+        cache: false,
+        crossDomain: true,
+        timeout: 3000, //3 second timeout
+        beforeSend: function() {
+            console.log('sending request');
+        },
+        success: function(data) {
+            console.log(data);
+            console.log('success!');
+            $('#alert').fadeIn();
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            alert('Error de conexión');
+            console.log('error!');
+        }
+    });
+    return request;
+}
+$(document).ready(function() {
+    $('#wifi').click(function() {
+        try {
+            WifiWizard.isWifiEnabled(win, fail);
+        } catch (err) {
+            alert("Plugin Error - " + err.message);
+        }
+
+    });
+
+    function win(e) {
+        if (e) {
+            alert("Wifi enabled already");
+        } else {
+            WifiWizard.setWifiEnabled(true, winEnable, failEnable);
+        }
+
+    }
+
+    function fail(e) {
+        alert("Error checking Wifi status");
+    }
+
+    function winEnable(e) {
+        alert("Wifi enabled successfully");
+    }
+
+    function failEnable(e) {
+        alert("Error enabling Wifi ");
+    }
+
+    $('#search').click(function() {
+        try {
+            WifiWizard.listNetworks(listHandler, fail);
+        } catch (err) {
+            alert("Plugin Error - " + err.message);
+        }
+
+    });
+
+    function listHandler(a) {
+        alert(a);
+    }
+
+    $('#scan').click(function() {
+        try {
+            WifiWizard.getScanResults({ numLevels: 1 }, listHandler1, fail);
+        } catch (err) {
+            alert("Plugin Error - " + err.message);
+        }
+
+    });
+
+    function listHandler1(a) {
+        alert(JSON.stringify(a));
+    }
+
+    $('#connect').click(function() {
+        try {
+            var config = WifiWizard.formatWPAConfig("Invitados", "invitado123");
+            WifiWizard.addNetwork(config, function() {
+                WifiWizard.connectNetwork("Invitados");
+            });
+        } catch (err) {
+            alert("Plugin Error - " + err.message);
+        }
+
+    });
+
+    function connectSuccess(e) {
+        alert("Connect success");
+    }
+    $('#onoff input').on('click', function() {
+        newOnoff();
+    });
+});
